7a1c999e555f87b4fd9058dc1804c0d9c95d90c8
jungeun-am <jungeun0054@gmail.com>
2025-01-09 16:06:48 +0900

    Merge branch 'nomain' into eun

--- Second/src/main/java/com/hhlb/controller/FrontController.java
+++ Second/src/main/java/com/hhlb/controller/FrontController.java
@@ -58,7 +58,7 @@ public class FrontController extends HttpServlet {
 		 */
 		
 		FileInputStream fis =
-				new FileInputStream("");
+				new FileInputStream("C:\\Users\\admin\\git\\secondPeoject\\Second\\src\\main\\java\\com\\hhlb\\controller\\mapping.properties");
 		
 		prop.load(fis);
 		
