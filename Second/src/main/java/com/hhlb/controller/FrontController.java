c946486cf5e54eb00362db2b3608832a58cfd83b
Lee Hyemee <svsvdvdv@naver.com>
2025-01-17 10:06:32 +0900

    1/17 혜미

--- Second/src/main/java/com/hhlb/controller/FrontController.java
+++ Second/src/main/java/com/hhlb/controller/FrontController.java
@@ -64,7 +64,7 @@ public class FrontController extends HttpServlet {
 		
 		FileInputStream fis =
 
-				new FileInputStream("C:\\Users\\admin\\git\\secondPeoject\\Second\\src\\main\\java\\com\\hhlb\\controller\\mapping.properties");
+				new FileInputStream("");
 
 
 		prop.load(fis);
